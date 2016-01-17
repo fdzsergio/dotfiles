ddb1e44a17d0d9659b1f0ea13d34cfc4cbd51fc3
Sergio Fernández <fdz.sergio@gmail.com>
2016-01-18 00:28:19 +0100

    update aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -5,24 +5,18 @@ alias ,,='cd ..'
 alias ,,,='cd ../..'
 
 # ls
-alias ls="ls -F"
+alias ls='ls -F'
+alias l='ls -lFh'
+alias la='ls -lAFh'
 
 # Git
 alias gsu='git submodule update --init --recursive'
 alias gs='git status'
-compdef _git gs=git-status
 alias gut='git'
 alias cleanup_branches='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 
 # Editor
-alias s='subl .'
-
-# Rails
-alias tlog='tail -f log/development.log'
-alias b='bundle exec'
-alias bake='bundle exec rake'
-alias rr='rbenv rehash'
-alias bm='bin/m'
+alias m='mate .'
 
 # Mac OS X
 alias o='open .'
@@ -34,3 +28,21 @@ alias $=' '
 
 # Xcode
 alias ddd='rm -rf ~/Library/Developer/Xcode/DerivedData'
+
+# Brew
+alias cask="brew cask"
+
+# Utilities
+alias t='tail -f'
+alias dud='du -d 1 -h'
+alias duf='du -sh *'
+alias fd='find . -type d -name'
+alias ff='find . -type f -name'
+
+# Files
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Make zsh know about hosts already accessed by SSH
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
